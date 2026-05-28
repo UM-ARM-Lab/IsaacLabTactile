a1dfcccc0e535b9a38392ce0b561edd0c344b174
Fan Yang <fanyangr@umich.edu>
2026-05-27 22:38:12 -0400

    add nut initial rotation randomness to make the task harder

--- source/isaaclab_tasks/isaaclab_tasks/direct/factory/factory_tasks_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/factory/factory_tasks_cfg.py
@@ -405,6 +405,7 @@ class NutThread(FactoryTask):
 
     # Held Asset (applies to all tasks)
     held_asset_pos_noise: list = [0.0, 0.003, 0.003]  # noise level of the held asset in gripper
+    held_asset_rot_noise: list = [0.0, 0.6, 0.0]  # has visualized to check this is the right direction
     held_asset_rot_init: float = -90.0
 
     # Reward.
